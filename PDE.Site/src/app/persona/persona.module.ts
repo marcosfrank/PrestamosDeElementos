e029381a2caee4998848364316fd096fc3247396
Agustin Bortolotto <agustin.bortolotto@neoris.com>
2017-07-10 17:44:10 -0300

    Se agregó persona.module

---
+++ PDE.Site/src/app/persona/persona.module.ts
@@ -0,0 +1,15 @@
+import { NgModule }                 from '@angular/core';
+import { BrowserModule }            from '@angular/platform-browser';
+import { CommonModule }             from '@angular/common';
+import { HttpModule }               from '@angular/http';
+import { FormsModule }              from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { PersonaListaComponent } from './persona-lista/persona-lista.component';
+import { Service }                  from '../app.service';
+
+@NgModule({
+    imports: [CommonModule, BrowserModule, HttpModule, FormsModule, RouterModule ],
+    declarations: [PersonaListaComponent],
+    providers: [ Service ]
+})
+export class PersonaModule { }
