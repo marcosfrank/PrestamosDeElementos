49020eaf7a60cf7d5a3781ec9e56d0fbcd2aeef5
Agustin Bortolotto <agustin.bortolotto@neoris.com>
2017-07-11 10:53:55 -0300

    Fix en persona-lista

--- PDE.Site/src/app/persona/persona.module.ts
+++ PDE.Site/src/app/persona/persona.module.ts
@@ -1,20 +1,21 @@
-import { NgModule }                 from '@angular/core';
-import { BrowserModule }            from '@angular/platform-browser';
-import { CommonModule }             from '@angular/common';
-import { HttpModule }               from '@angular/http';
-import { FormsModule }              from '@angular/forms';
-import { RouterModule }             from '@angular/router';
+import { NgModule }                         from '@angular/core';
+import { BrowserModule }                    from '@angular/platform-browser';
+import { CommonModule }                     from '@angular/common';
+import { HttpModule }                       from '@angular/http';
+import { FormsModule }                      from '@angular/forms';
+import { RouterModule }                     from '@angular/router';
+import { MdButtonModule, MdCheckboxModule } from '@angular/material';
 
-import { PersonaListaComponent }    from './persona-lista/persona-lista.component';
-import { PersonaCrearComponent }    from './crear.component';
-import { PersonaEditarComponent }   from './editar.component';
-//import { PersonaDetalleComponent }  from './detalle.component';
-//import { PersonaEliminarComponent } from './eliminar.component';
+import { PersonaListaComponent }            from './persona-lista/persona-lista.component';
+import { PersonaCrearComponent }            from './crear.component';
+import { PersonaEditarComponent }           from './editar.component';
+//import { PersonaDetalleComponent }        from './detalle.component';
+//import { PersonaEliminarComponent }       from './eliminar.component';
 
-import { Service }                  from '../app.service';
+import { Service }                          from '../app.service';
 
 @NgModule({
-    imports:      [ CommonModule, BrowserModule, HttpModule, FormsModule, RouterModule ],
+    imports:      [ CommonModule, BrowserModule, HttpModule, FormsModule, RouterModule, MdButtonModule, MdCheckboxModule ],
     declarations: [ PersonaListaComponent, PersonaCrearComponent, PersonaEditarComponent ],
     providers:    [ Service ]
 })
